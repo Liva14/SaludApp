2d0ae389c520ba5488c8bb728e42c7938b2a8a0f
Liva <liva@liva.com>
2023-11-03 12:58:08 +0100

    Update dia 03/11/23

--- app/src/main/java/com/pmdm/saludapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.pmdm.saludapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnhello: Button = findViewById(R.id.btnhello)
+        val etName: EditText = findViewById(R.id.etname)
+        btnhello.setOnClickListener {
+            //Log.i("Test", "UA")
+            val name = etName.text.toString()
+            Toast.makeText(this, "Hola $name!", Toast.LENGTH_LONG).show()
+        }
     }
 }
